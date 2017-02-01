9423f71e1b13f3b68e0cf091bb2edb05cab38c30
Emil <e.ruhwald@gmail.com>
2017-02-01 11:07:39 +0100

    Initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,18 @@
+/**
+ * Created by EmilRuhwald on 01-02-2017.
+ */
+public class HelloWorld {
+
+    public HelloWorld(){
+        display();
+    }
+
+    public static void main (String[] args) {
+        new HelloWorld();
+    }
+
+    private void display(){
+        System.out.println("What up");
+
+    }
+}
